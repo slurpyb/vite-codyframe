792292c685e175ab6c9e5b1e0682aaf9322e52c3
Jordan Sweeting <slurpyb@fastmail.com>
2024-04-13 22:50:40 +0800

    Cleanup comments

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,19 +2,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite';
 import  sourcemaps from 'rollup-plugin-sourcemaps';
 export default defineConfig({
-    // build: {
-    //     rollupOptions: {
-    //         input: {
-    //             main: 'src/index.html',
-    //             // main: resolve(__dirname, 'src/index.html'),
-    //         // nested: resolve(__dirname, 'src/index.html'),
-    //       },
-    //     //   output: {
-    //     //     dir: 'dist',
-    //     //     // entryFileNames: 'dist/index.html'
-    //     //   }
-    //     },
-    // },
     plugins: [sourcemaps()],
     root: 'src',
     build: {
